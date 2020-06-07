3ef9bd15eccfb8e9866053f67d60e122ddb6c681
Pablo Silva <pablossp@hotmail.com>
2020-06-07 14:20:43 -0700

    setting up backend

--- acnh-collection-album/src/components/Menu/index.js
+++ acnh-collection-album/src/components/Menu/index.js
@@ -1,8 +1,7 @@
-import React, {useState} from 'react'
+import React  from 'react'
 import {Logo} from '../Logo'
 import {UserInformation} from '../UserInformation'
 import {Nav, Title} from './styles'
-import {Button} from '@bootstrap-styled/v4'
 
 const DEFAULT_AVATAR_IMAGE_URL = "https://upload.wikimedia.org/wikipedia/commons/5/5b/Avatar_leaf.png"
 const LOGO_IMAGE_URL = "https://www.pcworld.es/cmsdata/slideshow/3786537/animal-crossing_guia-compra_thumb1200_4-3.jpg"
@@ -11,15 +10,8 @@ const LOGO_IMAGE_URL = "https://www.pcworld.es/cmsdata/slideshow/3786537/animal-
 
 
 export const Menu = props => {
-    
-    const [user, setUser] = useState("Guest")
-    
-    function handleClick () {
-        setUser("Pablo")
-    }
     return (<Nav>
         <Logo source={LOGO_IMAGE_URL}></Logo>
         <Title>Animal Crossing<br></br>Collection Album</Title>
-        <UserInformation avatar={DEFAULT_AVATAR_IMAGE_URL} name={user}></UserInformation>
-        <Button onClick={handleClick}>Nombre</Button>
+        <UserInformation avatar={DEFAULT_AVATAR_IMAGE_URL} name="Guest"></UserInformation>
     </Nav>)}
--- acnh-collection-album/src/components/UserInformation/index.js
+++ acnh-collection-album/src/components/UserInformation/index.js
@@ -1,16 +1,11 @@
-import React, {useState} from 'react'
+import React from 'react'
 import {Avatar, Username, Container} from './styles'
 
 export const UserInformation = props =>{
-   const [url, setUrl] = useState(props.avatar)
-   function handleChange(e) {
-       setUrl(e.target.value);
-   }
 return (
     <Container>
-        <Avatar src={url} alt="User avatar"></Avatar>
+        <Avatar src={props.avatar} alt="User avatar"></Avatar>
         <Username>{props.name}</Username>
-        <input onChange={handleChange}></input>
     </Container>
 )
 }
